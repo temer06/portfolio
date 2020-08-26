cfa26bc602dc620feaad00506be7d8dd2cb6e462
temer06 <61481236+temer06@users.noreply.github.com>
2020-08-26 08:13:27 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.projects__pagination',
+    bulletClass:'projects__bullet',
+    bulletActiveClass:'projects__bullet-activ',
+    clickable: true
+  },
+});
